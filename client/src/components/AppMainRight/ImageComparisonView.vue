1173b533bc70f321cbfb2f6cee8b997941af7f24
wakouboy <wakouboy@gmail.com>
2017-07-05 20:43:04 +0800

    Merge branch 'master' of https://github.com/swustlqsh/MC3-Multispectral chen # Conflicts: #	client/src/components/AppMainLeft/EventViewList.vue

--- client/src/components/AppMainRight/ImageComparisonView.vue
+++ client/src/components/AppMainRight/ImageComparisonView.vue
@@ -1,21 +1,83 @@
 <template>
-    <div class="uk-grid event-view-main">
-      <div class="uk-width-1-1">
-      </div>
+    <div id = 'timeLine'></div>
+    <div id = 'imageCompare'></div>
+    <div id = 'statistics'>
+        <div id = 'tagChannel'></div>
+        <div id = 'submmit'>
+            <div>
+                Type
+                <select style="margin-left: 20px" id = 'eventSelect'>
+                    <option value="Flood">Flood</option>
+                    <option value="Burnt">Burnt</option>
+                    <option value="Shrink">Shrink</option>
+                    <option value="Expand">Expand</option>
+                </select>
+            </div>
+            <div style="margin-top: 20px">
+                Description
+            </div>
+            <div>
+                <textarea id = 'commentsText' rows="7" cols="30">
+                </textarea>
+            </div>
+            <div style="text-align: center; margin-top: 20px">
+                <button type="button" id="sbutton"> Submit </button>
+            </div>
+        </div>
     </div>
 </template>
 <script>
+  import $ from 'jquery'
   export default {
     data () {
       return {
+        divName: 'image-comparison'
       }
     },
-    ready () {}
+    watch: {},
+    methods: {
+      init () {
+        $('#sbutton').click(function () {
+          console.log($('#commentsText').val(), $('#eventSelect').val())
+        })
+      },
+      submmitComments () {
+      }
+    },
+
+    ready () {
+      this.init()
+    }
   }
 </script>
 <style lang="less" scoped>
-  .event-view-main {
-    border: 1px solid gray;
-    padding: 5px 10px;
+  #timeLine {
+    width: 100%;
+    height: 10%;
+    background-color: #00d6b2;
+  }
+  #imageCompare {
+    width: 66.7%;
+    height: 90%;
+    background-color: #2d7091;
+  }
+  #statistics {
+    position: absolute;
+    top: 10%;
+    left: 66.7%;
+    width: 33.3%;
+    height: 90%;
+    background-color: #8a6343;
+  }
+  #tagChannel {
+    width: 50%;
+    height: 50%;
+  }
+  #submmit {
+    position: absolute;
+    width: 50%;
+    height: 50%;
+    top: 50%;
+    background-color: #659f13;
   }
 </style>
