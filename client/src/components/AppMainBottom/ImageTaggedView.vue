8ec23ccee51c80a7417034d89178de181238aacb
liqiusheng <liqiusheng@b.360.cn>
2017-07-07 15:36:26 +0800

    增加Graph交互

--- client/src/components/AppMainBottom/ImageTaggedView.vue
+++ client/src/components/AppMainBottom/ImageTaggedView.vue
@@ -12,7 +12,6 @@
       </ul>
     </div>
     <div class="uk-thumbnail uk-thumbnail-expand" v-el:graph>
-      <!--<img class="uk-thumbnail" src="../../../resource/3B/B1B5B6_2014_03_17.png" alt="">-->
       <canvas id="image_canvas"></canvas>
       <canvas id="region_canvas"></canvas>
     </div>
